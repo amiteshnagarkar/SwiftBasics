ce0e1ea96f00c5c4fe8e5ef380babafec3d62581
amiteshnagarkar <amiteshnagarkar@gmail.com>
2020-04-05 13:49:56 +0100

    Optional chaining & Enumeration

--- Contents.swift
+++ Contents.swift
@@ -113,6 +113,40 @@ if name != nil {
 
 //better method than force unwrapping
 //Optional binding
+var myOptional: String?
+myOptional = "Jon"
+if var myOptional = myOptional {
+myOptional = "test"
+print("Inside: \(myOptional)")
+}
+print("Outside: \(myOptional)")
+
+//Optional Chaining
+// allows us to call properties, methods & subscripts on an optional that might be nil. If any of the chained values return nil, the return value will be nil too.
+
+//Enumerations
+//special data types that allows us to group related types together.
+
+enum Product {
+case Book(Double, Int, Int)
+case Puzzle(Double, Int)
+}
+var masterSwift = Product.Book(49.99, 2017, 310)
+var worldPuzzle = Product.Puzzle(9.99, 200)
+
+switch masterSwift {
+case .Book(let price, let year, let pages):
+print("Mastering Swift was published in \(year) for the price of \(price) and has \(pages) pages")
+case .Puzzle(let price, let pieces):
+print("Mastering Swift is a puzzle with \(pieces) and sells for \(price)")
+}
+
+switch worldPuzzle {
+case .Book(let price, let year, let pages):
+print("World Puzzle was published in \(year) for the price of \(price) and has \(pages) pages")
+case .Puzzle(let price, let pieces):
+print("World Puzzle is a puzzle with \(pieces) and sells for \(price)")
+}
 
 
 
